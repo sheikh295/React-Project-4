ee941ad4327f96fe8eee042c17071b097bf2ca77
sheikh295 <sheikh295@users.noreply.github.com>
2023-08-14 19:10:32 +0500

    .

--- weather-app/src/Components/Result.jsx
+++ weather-app/src/Components/Result.jsx
@@ -5,19 +5,24 @@ export default function Result(props) {
     function KelvinToCelcius (k) {
         return (k - 273.15).toFixed(2) + '^C';
     }
+    function getTheDate(stamp) {
+        const date = new Date(stamp * 1000);
+        return date.toLocaleTimeString();
+    }
+
     if (data == "") {
         return (
             <>
                 <div className='row'>
-                <div className='col'>
-                    <div className='card border-primary'>
-                        <div className='card-body'>
-                            <h4 className='card-title'>City</h4>
-                            <p className='card-text temp-description'></p>
+                    <div className='col'>
+                        <div className='card border-primary'>
+                            <div className='card-body'>
+                                <h4 className='card-title'>City</h4>
+                                <p className='card-text temp-description'></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </>
         )
     }
@@ -31,6 +36,36 @@ export default function Result(props) {
                               <h4 className='card-title'>City {data.name}</h4>
                               <p className='card-text temp-description'>Temp: {KelvinToCelcius(data.main.temp)}</p>
                               <p className='card-text temp-description'><img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} /> {(data.weather[0].description).toUpperCase()}</p>
+                              <div className='row'>
+                                    <div className='col'>
+                                        <div className='row'>
+                                            <table className='table'>
+                                                <tbody>
+                                                    <tr>
+                                                        <th>Feels Like</th>
+                                                        <td> {KelvinToCelcius(data.main.feels_like)} </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Min. Temperature</th>
+                                                        <td> {KelvinToCelcius(data.main.temp_min)} </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Max. Temperature</th>
+                                                        <td> {KelvinToCelcius(data.main.temp_max)} </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Sun Rise</th>
+                                                        <td> {getTheDate(data.sys.sunrise)} </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Sun Set</th>
+                                                        <td> {getTheDate(data.sys.sunset)} </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
                           </div>
                       </div>
                   </div>
